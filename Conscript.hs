d6382c0594da7ce20ad4d9c2e997e83cd0d303f2
Lyndon Maydwell <lyndon@sordina.net>
2013-06-27 19:05:02 +1000

    Working... with hacks for blocking

--- Conscript.hs
+++ Conscript.hs
@@ -1,7 +1,8 @@
 import System.Environment
 import System.Process
 import System.Exit
-import Data.IORef
+import Control.Monad
+import Control.Concurrent
 
 main :: IO ()
 main = getArgs >>= options
@@ -14,18 +15,24 @@ options args         = run args
 
 run :: [String] -> IO ()
 run args = do
-  i <- newIORef =<< startProcess args
-  mapM_ (restart args i) . lines =<< getContents
+  c <- newChan
+  startProcess args >>= writeChan c
+  mapM_ (restart args c) . (filter (== '\n')) =<< getContents
+  block
 
 startProcess :: [String] -> IO ProcessHandle
 startProcess (h:t) = (\(_,_,_,ph) -> ph) `fmap` createProcess (proc h t)
 startProcess []    = help >> exitFailure
 
-restart :: [String] -> (IORef ProcessHandle) -> String -> IO ()
+restart :: [String] -> (Chan ProcessHandle) -> Char -> IO ()
 restart args pio _unused = do
-  p <- readIORef pio
+  p <- readChan pio
   terminateProcess p
-  startProcess args >>= writeIORef pio
+  void $ waitForProcess p
+  void $ startProcess args >>= writeChan pio
 
 help :: IO ()
 help = putStrLn "Usage: conscript command [args*]"
+
+block :: IO ()
+block = void $ newEmptyMVar >>= readMVar
