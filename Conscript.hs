bbb0fe1cff8765107287be02470ba4927ea1e13e
Lyndon Maydwell <lyndon@sordina.net>
2013-07-01 15:56:49 +1000

    simplifying restarts

--- Conscript.hs
+++ Conscript.hs
@@ -1,6 +1,7 @@
 import System.Environment
 import System.Process
 import System.Exit
+import System.IO
 import Control.Monad
 import Control.Concurrent
 
@@ -15,21 +16,28 @@ options args         = run args
 
 run :: [String] -> IO ()
 run args = do
-  c <- newChan
-  startProcess args >>= writeChan c
-  mapM_ (restart args c) . (filter (== '\n')) =<< getContents
-  readChan c >>= terminateProcess
+  hSetBuffering stdin NoBuffering
+  blocker <- newEmptyMVar
+  running <- newEmptyMVar
+  void $ forkIO $ forever $ starter args blocker running
+  mapM_ (killer blocker running) . (filter (== '\n')) =<< getContents
+
+killer :: MVar () -> MVar ProcessHandle -> Char -> IO ()
+killer blocker running _char = void $ takeMVar running >>= terminateProcess >> takeMVar blocker
+
+starter :: [String] -> MVar () -> MVar ProcessHandle -> IO ()
+starter args blocker running = do
+  putMVar blocker ()
+  p <- startProcess args
+  putMVar running p
+  code <- waitForProcess p
+  case code of ExitFailure 15 -> return () -- Killed!
+               ExitFailure i  -> putStrLn $ "Process [" ++ unwords args ++ "] failed with exit-status [" ++ show i ++ "]"
+               ExitSuccess    -> return ()
 
 startProcess :: [String] -> IO ProcessHandle
 startProcess (h:t) = (\(_,_,_,ph) -> ph) `fmap` createProcess (proc h t)
 startProcess []    = help >> exitFailure
-
-restart :: [String] -> (Chan ProcessHandle) -> Char -> IO ()
-restart args pio _unused = do
-  p <- readChan pio
-  terminateProcess p
-  void $ waitForProcess p
-  void $ startProcess args >>= writeChan pio
 
 help :: IO ()
 help = putStrLn "Usage: conscript command [args*]"
